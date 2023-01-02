a0a06e4d640ae08fad2b57b4ae87cde518c7d0da
arthurvteles <arthur.vteles@gmail.com>
2023-01-02 14:50:03 -0300

    add url about

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
 
-def my_view():
-    pass
+#HTTP request
+def my_view(request):
+    return HttpResponse('Hello World !')
 
-
-urlpatterns = [
+    urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', my_view),
 ]
+    
