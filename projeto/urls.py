6f4e88b429b8dbc290ed2a161958a5eb65fb85ff
arthurvteles <arthur.vteles@gmail.com>
2023-01-02 14:37:31 -0300

    add url about/

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def my_view():
+    pass
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', my_view),
 ]
