f2ad9ca284f9532ef0dbd731a7898ee313554773
arthurvteles <arthur.vteles@gmail.com>
2023-01-28 09:13:36 -0300

    curso django

--- recipes/apps.py
+++ recipes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecipiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recipies'
+    name = 'recipes'
--- recipes/views.py
+++ recipes/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def Home(request): 
-    return HttpResponse('Página Inicial') 
+def Home(request):
+    return render(request, 'recipes/home.html', context={'name': 'Arthur'})
 
 
 def Contact(request):
     return HttpResponse('(62)98436-8603')
 
 
-def About(request): 
+def About(request):
     return HttpResponse('Somos uma empresa criada em 2008...')
