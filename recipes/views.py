6846e5ba3d0fe7a5417d3761acf8f2e6ed6b2842
arthurvteles <arthur.vteles@gmail.com>
2023-04-21 16:07:59 -0300

    Limpeza de código

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,10 +5,3 @@ from django.shortcuts import render
 def Home(request):
     return render(request, 'recipes/home.html', context={'name': 'Arthur'})
 
-
-def Contact(request):
-    return HttpResponse('(62)98436-8603')
-
-
-def About(request):
-    return HttpResponse('Somos uma empresa criada em 2008...')
